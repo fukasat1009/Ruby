503046377a355256aa6d0e9f3fa804882fa66909
津田恭平 <tsudakyouhei@kumadon.local>
2020-04-20 21:14:10 +0900

    [finish]2-7

---
+++ lesson6.rb
@@ -0,0 +1,12 @@
+total_price = 120
+
+if total_price ==100
+	puts "みかんを購入。所持金は0円"
+end
+
+ if total_price >=100
+    	puts "みかんを購入。所持金にあまりあり"
+  end
+   if total_price <=100
+    	puts "みかんを購入することはできません"
+end
